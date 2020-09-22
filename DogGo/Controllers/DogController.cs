8dc843aeaffd523c7af4bddb3a58bc97a9a35c66
Lacey Walker <bluelarksong@gmail.com>
2020-09-21 23:04:42 -0500

    saving progress, got the dogcontroller and idogrepository going

---
+++ DogGo/Controllers/DogController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DogGo.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DogGo.Controllers
+{
+    public class DogController : Controller
+    {
+        private readonly IDogRepository _dogRepository;
+        public DogController(IDogRepository dogRepository)
+        {
+            _dogRepository = dogRepository;
+        }
+        public IActionResult Index()
+        {
+            List<dog>
+            return View();
+        }
+    }
+}
---
+++ DogGo/Repositories/IDogRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DogGo.Models;
+
+
+namespace DogGo.Repositories
+{
+    public class IDogRepository : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+
+        }
+    }
+}
+

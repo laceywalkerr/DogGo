46ecbbdd6e89d7a2b25ed6589f82a90d942e79ed
Lacey Walker <bluelarksong@gmail.com>
2020-09-23 23:08:09 -0500

    tried adding some code to WalksController and IWalkRepository. Commented it out because I havent set up the WalkRepository yet and it would break. Really want do do this soon, I hate when things are undone.

--- DogGo/Repositories/IWalkRepository.cs
+++ DogGo/Repositories/IWalkRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DogGo.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,9 @@ namespace DogGo.Repositories
 {
     public class IWalkRepository
     {
+
+/*        List<Walk> GetAllWalks();
+        Walk GetWalkByDetails(int id);*/
+
     }
 }
